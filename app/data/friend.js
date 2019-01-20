3c4f008eeb918d4f69a507d65b38bca99f9a0326
HFMarley <hankfoxmarley@gmail.com>
2019-01-20 14:42:32 -0600

    added files and content

---
+++ app/data/friend.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+  name: "Hank",
+  photo: "https//media.test",
+  scores: [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+  ]
+  },
+]
